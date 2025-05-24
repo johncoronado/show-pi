9d55c4ad2fdcaf986c2224534a9f4f1c33d2cbd4
John Coronado <66081204+johncoronado@users.noreply.github.com>
2025-05-23 21:43:43 -0700

    Motd (#5)
    * Updated gitignore and added motd logo base.
    
    * Set script now updates a motd with logo and good login info

--- setup.sh
+++ setup.sh
@@ -26,4 +26,21 @@ while IFS= read -r script_path; do
   fi
 done < "$ORDER_FILE"
 
+# Updates the motd file for good info on login and copies it to /etc/motd
+cp $HOME/show-pi/config-files/logo $HOME/show-pi/config-files/motd
+
+MOTD_FILE="$HOME/show-pi/config-files/motd"
+
+{
+  echo ""
+  echo -e "Welcome, \033[1;33m$(logname)\033[0m"
+  echo -e "Hostname: \033[1;33m$(hostname)\033[0m"
+  echo -e "To control Ontime, \033[1;33mhttp://$(hostname).local:4001/editor\033[0m"
+  echo -e "To control Companion, \033[1;33mhttp://$(hostname).local:8000\033[0m"
+  echo -e "To play images, navigate to the network share and place an image in the show-images folder."
+  echo -e "It is recommended that you bookmark these websites for fast access.\n"
+} >> "$MOTD_FILE"
+
+sudo cp $MOTD_FILE /etc/motd
+echo -e "\nMOTD file has been updated"
 echo -e "\n\033[1;31mSetup complete. Please reboot your system!\033[0m\n"
