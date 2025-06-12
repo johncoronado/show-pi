132d9883b39abb9121bfff89632a73a8855f733e
John Coronado <66081204+johncoronado@users.noreply.github.com>
2025-06-11 22:51:49 -0700

    Added companion update so user can select version

--- scripts/companion-setup.sh
+++ scripts/companion-setup.sh
@@ -12,7 +12,8 @@ echo -e -n  "\n\033[1m"Install Companion?"\033[0m (y/n): "
 	chmod +x install.sh
 
 	# Sets stable build and runs the script with root privileges using bash
-	sudo COMPANION_BUILD="v4.0.0" bash install.sh
+	sudo COMPANION_BUILD=stable bash install.sh
+    sudo companion-update
 
 	# Removes the script from show-pi directory
 	rm install.sh
